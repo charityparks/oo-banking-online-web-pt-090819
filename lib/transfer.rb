c5ac1d5239c6e9d629b849d5d3291ce2696ed086
Charity Parks <tuppercharity@gmail.com>
2019-10-10 23:30:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,23 @@ class Transfer
   end
   
   def execute_transaction
-    
-    
+    if self.status == "pending" && valid? && sender.balance > amount
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
+
+  def reverse_transfer
+    if valid? && self.status == "complete"
+      sender.balance += self.amount
+      receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
+    
+  
 end
